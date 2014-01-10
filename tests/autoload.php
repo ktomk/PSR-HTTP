8b23237564ac356041361c3d4e3f0553248fdd94
Tom <tklingenberg@lastflood.net>
2014-01-11 00:34:01 +0100

    Init Test with autoloader

---
+++ tests/autoload.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * autoloader for all tests
+ */
+
+require __DIR__ . '/../src/Psr/Http/MessageInterface.php';
+require __DIR__ . '/../src/Psr/Http/RequestInterface.php';
+require __DIR__ . '/../src/Psr/Http/ResponseInterface.php';
