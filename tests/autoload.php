fa4d614181d3949d4042eeeefbb92242c630f84d
Tom <tklingenberg@lastflood.net>
2014-01-11 13:14:14 +0100

    Updated dependencies and implementation to reflect changes

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,7 +3,6 @@
  * autoloader for all tests
  */
 
-require __DIR__ . '/../src/Psr/Http/HasHeadersInterface.php';
 require __DIR__ . '/../src/Psr/Http/StreamInterface.php';
 require __DIR__ . '/../src/Psr/Http/MessageInterface.php';
 require __DIR__ . '/../src/Psr/Http/RequestInterface.php';
--- tests/fixtures/ConcreteFirstInterfaceImplementation.php
+++ tests/fixtures/ConcreteFirstInterfaceImplementation.php
@@ -6,121 +6,6 @@ use Psr\Http\StreamInterface;
 class ConcreteFirstInterfaceImplementation implements MessageInterface
 {
     /**
-     * Gets all headers.
-     *
-     * The keys of the returned array represents the header name as it will be
-     * sent over the wire, and each value is a HeaderValuesInterface object that
-     * can be used like an array or cast to a string.
-     *
-     *     // Represent the headers as a string
-     *     foreach ($message->getHeaders() as $name => $values) {
-     *         echo "{$name}: {$values}\r\n";
-     *     }
-     *
-     * @return array Returns an associative array of the message's headers
-     */
-    public function getHeaders()
-    {
-        // TODO: Implement getHeaders() method.
-    }
-
-    /**
-     * Checks if a header exists by the given case-insensitive name.
-     *
-     * @param string $header Case-insensitive header name.
-     *
-     * @return bool Returns true if any header names match the given header
-     *              name using a case-insensitive string comparison. Returns
-     *              false if no matching header name is found in the message.
-     */
-    public function hasHeader($header)
-    {
-        // TODO: Implement hasHeader() method.
-    }
-
-    /**
-     * Retrieve a header by name.
-     *
-     * @param string $header Header name.
-     *
-     * @return \Psr\Http\HeaderValuesInterface|null Returns the header values or or null
-     *                                    if not set.
-     */
-    public function getHeader($header)
-    {
-        // TODO: Implement getHeader() method.
-    }
-
-    /**
-     * Sets a header, replacing any existing values of any headers with the
-     * same case-insensitive name.
-     *
-     * The header values MUST be a string, an array of strings, or a
-     * HeaderValuesInterface object.
-     *
-     * @param string $header Header name
-     * @param string|array|\Psr\Http\HeaderValuesInterface $value Header value(s)
-     *
-     * @return self Returns the message.
-     */
-    public function setHeader($header, $value)
-    {
-        // TODO: Implement setHeader() method.
-    }
-
-    /**
-     * Sets headers, replacing any headers that have already been set on the
-     * message.
-     *
-     * The array keys MUST be a string. The array values must be either a
-     * string, array of strings, or a HeaderValuesInterface object.
-     *
-     * @param array $headers Headers to set.
-     *
-     * @return self Returns the message.
-     */
-    public function setHeaders(array $headers)
-    {
-        // TODO: Implement setHeaders() method.
-    }
-
-    /**
-     * Appends a header value to any existing values associated with the
-     * given header name.
-     *
-     * @param string $header Header name to add
-     * @param string $value Value of the header
-     *
-     * @return self
-     */
-    public function addHeader($header, $value)
-    {
-        // TODO: Implement addHeader() method.
-    }
-
-    /**
-     * Remove a specific header by case-insensitive name.
-     *
-     * @param string $header HTTP header to remove
-     *
-     * @return self
-     */
-    public function removeHeader($header)
-    {
-        // TODO: Implement removeHeader() method.
-    }
-
-    /**
-     * Returns a string representation of the HTTP message.
-     *
-     * @return string Message as a string.
-     */
-    public function __toString()
-    {
-        // TODO: Implement __toString() method.
-    }
-
-    /**
      * Gets the HTTP protocol version.
      *
      * @return string HTTP protocol version.
@@ -157,5 +42,127 @@ class ConcreteFirstInterfaceImplementation implements MessageInterface
         throw new InvalidArgumentException('The body is not valid');
         // TODO: Implement setBody() method.
     }
-}
 
+    /**
+     * Gets all headers.
+     *
+     * The keys of the returned array represents the header name as it will be
+     * sent over the wire, and each value is a HeaderFieldValuesInterface object
+     * that can be used like an array or cast to a string.
+     *
+     *     // Represent the headers as a string
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         echo "{$name}: {$values}\r\n";
+     *     }
+     *
+     * @return array Returns an associative array of the message's headers
+     */
+    public function getHeaders()
+    {
+        // TODO: Implement getHeaders() method.
+    }
+
+    /**
+     * Checks if a header exists by the given case-insensitive name.
+     *
+     * @param string $header Case-insensitive header name.
+     *
+     * @return bool Returns true if any header names match the given header
+     *              name using a case-insensitive string comparison. Returns
+     *              false if no matching header name is found in the message.
+     */
+    public function hasHeader($header)
+    {
+        // TODO: Implement hasHeader() method.
+    }
+
+    /**
+     * Retrieve a header by name.
+     *
+     * @param string $header Header name.
+     *
+     * @return \Psr\Http\HeaderFieldValuesInterface|null Returns the header values or or
+     *                                         null if not set.
+     */
+    public function getHeader($header)
+    {
+        // TODO: Implement getHeader() method.
+    }
+
+    /**
+     * Sets a header, replacing any existing values of any headers with the
+     * same case-insensitive name.
+     *
+     * The header values MUST be a string, an array of strings, or a
+     * HeaderFieldValuesInterface object.
+     *
+     * @param string $header Header name
+     * @param string|array|\Psr\Http\HeaderFieldValuesInterface $value Header value(s)
+     *
+     * @return self Returns the message.
+     */
+    public function setHeader($header, $value)
+    {
+        // TODO: Implement setHeader() method.
+    }
+
+    /**
+     * Sets headers, replacing any headers that have already been set on the
+     * message.
+     *
+     * The array keys MUST be a string. The array values must be either a
+     * string, array of strings, or a HeaderFieldValuesInterface object.
+     *
+     * @param array $headers Headers to set.
+     *
+     * @return self Returns the message.
+     */
+    public function setHeaders(array $headers)
+    {
+        // TODO: Implement setHeaders() method.
+    }
+
+    /**
+     * Appends a header value to any existing values associated with the
+     * given header name.
+     *
+     * @param string $header Header name to add
+     * @param string $value Value of the header
+     *
+     * @return self
+     */
+    public function addHeader($header, $value)
+    {
+        // TODO: Implement addHeader() method.
+    }
+
+    /**
+     * Merges in an associative array of headers.
+     *
+     * Each array key MUST be a string representing the case-insensitive name
+     * of a header. Each value MUST be either an array of strings or an array
+     * of HeaderFieldValuesInterface objects. For each value, the value is
+     * appended to any existing header of the same name, or, if a header does
+     * not already exist by the given name, then the header is added.
+     *
+     * @param array $headers Associative array of headers to add to the message
+     *
+     * @return self
+     */
+    public function addHeaders(array $headers)
+    {
+        // TODO: Implement addHeaders() method.
+    }
+
+    /**
+     * Remove a specific header by case-insensitive name.
+     *
+     * @param string $header HTTP header to remove
+     *
+     * @return self
+     */
+    public function removeHeader($header)
+    {
+        // TODO: Implement removeHeader() method.
+    }
+}
